d619dfd1fc1b24c69c0f43bb7da25eb607d0cdcc
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-17 22:39:04 +0530

    Data Types | String | Number

---
+++ 1.0_2.js
@@ -0,0 +1,38 @@
+//String    [Black Color in O/P]
+let name1="chethan"
+let initial1='D'
+let initial2='B'
+let nmae2="naik"
+let name3="abshiek"
+let name4="Aryakanth"
+let name5="virat"
+
+console.log("my full name is",name1,initial1,initial2)
+console.log("my first frd ",nmae2)
+console.log("my second frd",name3)
+console.log("my remaining frd are",name4,name5)
+
+console.log("hello my company")
+console.log("name is 'DCL'")
+
+console.log("my full name is",name1,initial1,initial2,"\nmy first frd",nmae2,"my second frd",name3,"my remaining frd",name4,name5)
+
+console.log(`hello my full name is ${name1} ${initial1} ${initial2},\n my first friend is ${nmae2}, \n my second friend is ${name3},\n my Remaining freinds are ${name4} and ${name5}`)
+console.log(typeof(name1))
+
+///////////////////////////
+//Number    [Blue Color in O/P]
+let a=100.11
+console.log(a) //100.11
+console.log(typeof(a)) //number
+console.log(parseInt(a))//100
+let b=99;
+console.log(parseFloat(b))//99
+console.log(typeof(typeof(b)))//String
+console.log(String(b)) //99
+console.log(typeof(String(b))) //String
+c=100
+console.log(c) //100
+console.log(typeof(c)) //Number
+console.log(Number(c)) //100
+console.log(parseInt(c)) //100
