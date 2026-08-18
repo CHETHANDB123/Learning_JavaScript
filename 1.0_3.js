73030effd59069671f232fb941902278e7a379ad
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-18 22:08:52 +0530

    Boolean

---
+++ 1.0_3.js
@@ -0,0 +1,40 @@
+//Boolean
+let a3 = true
+console.log(a3)
+console.log(typeof(a3))
+let a4=false
+console.log(a4)
+console.log(typeof(a4))
+let a5=!false
+console.log(a5)
+let a6=!true
+console.log(a6)
+let a7=Boolean(true)
+console.log(a7)
+let a8=Boolean(false)
+console.log(a8)
+
+let a9=Boolean(1)
+console.log(a9)
+let b1=Boolean(0)
+console.log(b1)
+
+let b2=Boolean("0")
+console.log(b2)
+let b3=Boolean("chethan")
+console.log(b3)
+
+let b4=Boolean(" ")
+console.log(b4)
+let b5=Boolean(Number(0))
+console.log(b5)
+let b6=Boolean(Number(1))
+console.log(b6)
+let b7=Boolean(Number("chethan"))
+console.log(b7)
+
+let b8=Boolean(isNaN("che"))
+console.log(b8)
+console.log(null)
+console.log(undefined)
+console.log(Boolean(String(Number("chethan"))))
