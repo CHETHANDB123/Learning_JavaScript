843215af83005ef20ecd111af26a51a06349ebb1
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-20 22:47:56 +0530

    Number Method | Date | Math Method | Generate OTP

---
+++ 1.0_5.js
@@ -0,0 +1,45 @@
+//number method
+// let a=100.500001
+// console.log(a)
+// console.log(a.toExponential())
+// console.log(a.toFixed(4))
+// console.log(a.toPrecision(2)) #1.0e+2
+// console.log(a.toString())
+
+///date
+// let date= new Date;
+// console.log(date)
+// console.log(date.getSeconds())
+// console.log(date.getFullYear())
+// console.log(date.getHours())
+// console.log(date.getSeconds())
+// console.log(date.getMilliseconds())
+// console.log(date.getTime())
+// console.log(date.getDay())
+// console.log(date.getMonth())
+// console.log(date.toString())
+
+///Math Method
+// let a=100.9;
+// lat b=1000;
+// console.log(Math.abs(a))
+// console.log(Math.cbrt(b))
+// console.log(Math.aqrt(b))
+// console.log(Math.ceil(a))
+// console.log(Math.floor(a))
+// console.log(Math.max[1,2,3,4,5,65])
+// console.log(Math.min[0,1,2,3,4,5])
+// console.log(Math.floor(Math.random()*100))
+// write a program to generate random number for dice
+let a=Math.floor(Math.random()*10)
+let b=Math.floor(Math.random()*10)
+let c=Math.floor(Math.random()*10)
+let d=Math.floor(Math.random()*10)
+let e=Math.floor(Math.random()*10)
+console.log(a,b,c,d,e)
+
+
+
+
+
+
