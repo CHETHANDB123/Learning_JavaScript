9b84137a65aab80ae8b95126a2bd80bf3e20c405
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-21 21:42:12 +0530

    Arrays

---
+++ 1.0_6.js
@@ -0,0 +1,17 @@
+//display 5 fruits in array
+//ARRAYS
+// let arr=["apple","grapes","banana","mango","watermelon"]
+// console.log(arr)
+// console.log(arr[0])
+// let che=["chethan","D B",1223344,22,"Male","DCL"]
+// console.log(`hello I am ${[0]} ${che[1]} ${che[2]}${che[3]}${che[4]}Internship in ${che[5]}`)
+let movies=['Toxic','Legend','Leo','Devil','Salaar']
+let hero=['Darshan','Vijay','Balayya','prabs','Yash']
+let heroin=['kiara','sonal','trisha','rachana','shruthi hassan']
+let year=[2026,2014,2023,2025,2024]
+console.log(`Movie: ${movies[0]}\nHero: ${hero[4]}\nHeroin: ${heroin[0]}\nYear: ${year[0]}`)
+console.log(`Movie: ${movies[1]}\nHero: ${hero[2]}\nHeroin: ${heroin[1]}\nYear: ${year[1]}`)
+console.log(`Movie: ${movies[2]}\nHero: ${hero[1]}\nHeroin: ${heroin[2]}\nYear: ${year[2]}`)
+console.log(`Movie: ${movies[3]}\nHero: ${hero[0]}\nHeroin: ${heroin[3]}\nYear: ${year[3]}`)
+console.log(`Movie: ${movies[4]}\nHero: ${hero[3]}\nHeroin: ${heroin[4]}\nYear: ${year[4]}`)
+
