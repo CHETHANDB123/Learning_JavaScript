6e45ac39dc9b9f072400d1166790821f979bc6fb
CHETHAN D B <chethandb.cse@gmail.com>
2026-08-27 19:13:29 +0530

    dictionary{  }

---
+++ 1.0_9.js
@@ -0,0 +1,201 @@
+//Object
+// let me ={
+//     name:"chethan",
+//     age:22,
+//     gender:"Male",
+//     Quali:"BE",
+//     company:"DCL",
+//     Course:"Python Full Stack",
+//     address:"Bangalore",
+//     phone:"Poco",
+//     height:6,
+//     email:"chethandb@gmail.com"
+// }
+// console.log(me)
+// console.log(`name: ${me.name}`)
+// console.log(`age: ${me.age}`)
+// console.log(`gender: ${me.gender}`)
+// console.log(`Quali: ${me.Quali}`)
+// console.log(`company: ${me.company}`)
+// console.log(`course: ${me.course}`)
+// console.log(`address: ${me.address}`)
+// console.log(`phone: ${me.phone}`)
+// console.log(`height: ${me.height}`)
+// console.log(`email: ${me.email}`)
+
+
+// phone laptop bike collage  school family house_model ravi
+//Mobile
+let mobile={
+    name:"POCO",
+    year:2024,
+    RAM:'8GB',
+    storage: '256GB',
+    color:"Black",
+    Price:20000,
+    version:"X5 pro",
+    purchase:"Flipkart"
+}
+console.log(`name: ${mobile.name}`)
+console.log(`year: ${mobile.year}`)
+console.log(`RAM: ${mobile.RAM}`)
+console.log(`storage: ${mobile.storage}`)
+console.log(`color: ${mobile.color}`)
+console.log(`price: ${mobile.Price}`)
+console.log(`version: ${mobile.version}`)
+console.log(`purchase: ${mobile.purchase}`)
+
+//Laptop
+let laptop={
+    name:"TUF",
+    year:2024,
+    RAM:'16GB',
+    storage: '1TB',
+    color:"Black",
+    Price:80000,
+    version:"11 Windows",
+    purchase:"Reliance"
+}
+console.log(`name: ${laptop.name}`)
+console.log(`year: ${laptop.year}`)
+console.log(`RAM: ${laptop.RAM}`)
+console.log(`storage: ${laptop.storage}`)
+console.log(`color: ${laptop.color}`)
+console.log(`price: ${laptop.Price}`)
+console.log(`version: ${laptop.version}`)
+console.log(`purchase: ${laptop.purchase}`)
+
+//Bike
+let bike={
+    name:"Himalaya",
+    year:2027,
+    milage:20,
+    engine:"V4",
+    color:"White",
+    Price:400000,
+    Wheels:2,
+    purchase:"showroom"
+}
+console.log(`name: ${bike.name}`)
+console.log(`year: ${bike.year}`)
+console.log(`milage: ${bike.milage}`)
+console.log(`engine: ${bike.engine}`)
+console.log(`color: ${bike.color}`)
+console.log(`price: ${bike.Price}`)
+console.log(`wheels: ${bike.wheels}`)
+console.log(`purchase: ${bike.purchase}`)
+
+//College
+let collage={
+    name:"AIET",
+    year:2022,
+    education:"BE",
+    branch:"CSE",
+    address:"Mangalore",
+    NACC:"A",
+    duartion:"4-years",
+    under:"VTU"
+}
+console.log(`name: ${collage.name}`)
+console.log(`year: ${collage.year}`)
+console.log(`education: ${collage.education}`)
+console.log(`branch: ${collage.branch}`)
+console.log(`address: ${collage.address}`)
+console.log(`NACC: ${collage.NACC}`)
+console.log(`duration: ${collage.duartion}`)
+console.log(`under: ${collage.under}`)
+
+
+//School
+let school={
+    name:"stella maris school",
+    year:2020,
+    education:"1-10",
+    total_years:10,
+    address:"honnali",
+    borad:"state",
+    duartion:"9-4",
+    under:"Public school"
+}
+console.log(`name: ${school.name}`)
+console.log(`year: ${school.year}`)
+console.log(`education: ${school.education}`)
+console.log(`total_years: ${school.total_years}`)
+console.log(`address: ${school.address}`)
+console.log(`board: ${school.board}`)
+console.log(`duration: ${school.duartion}`)
+console.log(`under: ${school.under}`)
+
+
+// family
+let family={
+    name:"D B",
+    palce:"Daganakatte",
+    members:4,
+    sister:1,
+    dad:"teacher",
+    mom:"teacher",
+    under:"GOVT",
+    childern:2
+}
+console.log(`name: ${family.name}`)
+console.log(`place: ${family.place}`)
+console.log(`members: ${family.members}`)
+console.log(`sister: ${family.sister}`)
+console.log(`dad: ${family.dad}`)
+console.log(`mom: ${family.mom}`)
+console.log(`under: ${family.under}`)
+console.log(`children: ${family.childern}`)
+
+//House
+let house={
+    name:"sri baveshwara nilaya",
+    year:2011,
+    place:"Honnali",
+    district:"Davanagere",
+    color:"White",
+    floor:"Ground floor",
+    state:"karnataka",
+    rooms:3
+}
+console.log(`name: ${house.name}`)
+console.log(`year: ${house.year}`)
+console.log(`place: ${house.place}`)
+console.log(`district: ${house.district}`)
+console.log(`color: ${house.color}`)
+console.log(`floor: ${house.floor}`)
+console.log(`state: ${house.state}`)
+console.log(`rooms: ${house.rooms}`)
+
+//ravi
+let ravi={
+    name:"ravishastry",
+    year:2026,
+    age:30,
+    education:"BE",
+    company:"DCL",
+    job:"Front-end",
+    vehicle:"car",
+    class:"90 minutes"
+}
+console.log(`name: ${ravi.name}`)
+console.log(`year: ${ravi.year}`)
+console.log(`age: ${ravi.age}`)
+console.log(`education: ${ravi.education}`)
+console.log(`company: ${ravi.company}`)
+console.log(`job: ${ravi.job}`)
+console.log(`vehicle: ${ravi.vehicle}`)
+console.log(`class: ${ravi.class}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
