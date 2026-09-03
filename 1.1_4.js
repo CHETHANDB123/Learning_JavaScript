73fcf36c62dc9b6cf87f198abba7705d606f0dcb
CHETHAN D B <chethandb.cse@gmail.com>
2026-09-03 23:13:03 +0530

    Arithmetic Operators

---
+++ 1.1_4.js
@@ -0,0 +1,92 @@
+//OPERATORS
+//ARITHEMATIC OPERATORS
+let a1=100
+let b=200
+console.log(a1+b)
+console.log(a1-b)
+console.log(a1*b)
+console.log(a1/b)
+console.log(a1%b)
+console.log(a1**b)
+
+
+let a11="100"
+let b1=100
+console.log(a11+b1)
+console.log(a11-b1)
+console.log(a11*b1)
+console.log(a11/b1)
+console.log(a11%b1)
+console.log(a11**b1)
+
+let a2="100"
+let b2="100"
+console.log(a2+b2)
+console.log(a2-b2)
+console.log(a2*b2)
+console.log(a2/b2)
+console.log(a2%b2)
+console.log(a2**b2)
+
+let a3="ravi"
+let b3=100
+console.log(a3+b3)
+console.log(a3-b3)
+console.log(a3*b3)
+console.log(a3/b3)
+console.log(a3%b3)
+console.log(a3**b3)
+
+let a4="100a"
+let b4=100
+console.log(a4+b4)
+console.log(a4-b4)
+console.log(a4*b4)
+console.log(a4/b4)
+console.log(a4%b4)
+console.log(a4**b4)
+
+let a5="undefined"
+let b5=100
+console.log(a5+b5)
+console.log(a5-b5)
+console.log(a5*b5)
+console.log(a5/b5)
+console.log(a5%b5)
+console.log(a5**b5)
+
+let a6=undefined
+let b6=null
+console.log(a6+b6)
+console.log(a6-b6)
+console.log(a6*b6)
+console.log(a6/b6)
+console.log(a6%b6)
+console.log(a6**b6)
+
+let a7=true
+let b7=true
+console.log(a7+b7)
+console.log(a7-b7)
+console.log(a7*b7)
+console.log(a7/b7)
+console.log(a7%b7)
+console.log(a7**b7)
+
+let a8="true"
+let b8=true
+console.log(a8+b8)
+console.log(a8-b8)
+console.log(a8*b8)
+console.log(a8/b8)
+console.log(a8%b8)
+console.log(a8**b8)
+
+let a9=null
+let b9=null
+console.log(a9+b9)
+console.log(a9-b9)
+console.log(a9*b9)
+console.log(a9/b9)
+console.log(a9%b9)
+console.log(a9**b9)
