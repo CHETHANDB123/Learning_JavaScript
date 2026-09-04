3c268bf2e4024f5a205932ddb5caaac79398b854
CHETHAN D B <chethandb.cse@gmail.com>
2026-09-04 23:51:21 +0530

    Assignment Operator

---
+++ 1.1_5.js
@@ -0,0 +1,40 @@
+// Assignment Operator
+//pre-increment , post-increment
+//pre-decrement , post-decrement
+// let a =100
+// console.log(a) //100
+// console.log(a++) //100 //happening
+// // console.log(a++) //101
+// console.log(a) //101
+
+// let x=100
+// console.log(x++) //100
+// console.log(++x) //102
+// console.log(x--)//102
+// console.log(x--)//101
+
+// let s=100
+// console.log(s++)
+// console.log(--s)
+// // console.log(++s)
+// console.log(s--)
+// console.log(s)
+
+//////////////////////
+// {
+//     var a="ravi"
+// }
+// console.log(a)
+
+// {
+//     let b="che"
+// }
+// console.log(b)
+
+// {
+//     const c="aaa"
+// }
+// console.log(c)
+
+/////////////////////////////
+
