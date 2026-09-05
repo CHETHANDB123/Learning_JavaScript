f59910d9da65984bd94dd4cf8141867692072a34
CHETHAN D B <chethandb.cse@gmail.com>
2026-09-05 20:28:25 +0530

    Comparision_Opeartor |  Function

---
+++ 1.1_6.js
@@ -0,0 +1,59 @@
+// //Comparision Opeartor
+let a="100"
+let b=200
+console.log(a<b)
+console.log(a>b)
+console.log(a<=b)
+console.log(a>=b)
+console.log(a!=b)
+console.log(a==b)
+
+let f=["a","s","d","f"]
+let i=Math.floor(Math.random()*f.length)
+let w=Math.floor(Math.random()*f.length)
+let p=Math.floor(Math.random()*f.length)
+console.log(f[i],f[w],f[p])
+// ///////////////////////////
+
+// // function Wedding() {
+// //     console.log("Hello")
+// // }
+// // Wedding()
+// // Wedding()
+// // Wedding()
+// // Wedding()
+// // Wedding()
+// // Wedding()
+// // Wedding()
+
+// //werite to grate person
+// function greet(){
+//     console.log("Welcome to Party")
+// }
+// greet()
+// /// bio data you name,address,colage name ,you , cgpa, 
+
+// function details(){
+//     console.log("name:Chethan")
+//     console.log("adress:Mangalore")
+//     console.log("collage:AIET")
+//     console.log("YOR:2026")
+//     console.log("CGPA:9")
+//     console.log("Branch:CSE")
+//     console.log("YOJ:2022")
+//     console.log("age:22")
+//     console.log("Duration:4-Years")
+//     console.log("Education:BE")
+//     console.log("Phone:1234567890")
+// }
+// details()
+
+// function add(a=1,b=2){
+//     let c=a+b
+//     console.log(c)
+// }
+// add()
+// add(a=10,b=20)
+// add(a=100,b=100)
+// add(a=1000,b=1000)
+// add(a=10000,b=40000)
