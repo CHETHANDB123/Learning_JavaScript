198ad6f68c18a748d2953df3c04e739a8821a95d
CHETHAN D B <chethandb.cse@gmail.com>
2026-09-06 23:27:51 +0530

    Functions (  )

---
+++ 1.1_7.js
@@ -0,0 +1,158 @@
+// function me1(){
+//     console.log("name:chethan")
+//     console.log("age:22")
+//     console.log("height:6")
+//     console.log("Education:BE")
+//     console.log("Collage:AIET")
+//     console.log("CGPA:8.3")
+//     console.log("Gender:Male")
+//     console.log("phone:1234567890")
+//     console.log("Branch:CSE")
+//     console.log("address:Shivamogga")
+// }
+// me1()
+// /////////////
+
+// function add(a=10,b=20){
+//     let c=a+b
+//     console.log(c)
+// }
+// add()
+// function sub(a=20,b=30){
+//     let c=b-a
+//     console.log(c)
+// }
+// sub()
+// function multi(a=Number(prompt("Enetr a multi number:"),b=Number(prompt("Enter a multi Value:")))){
+//     let c=a*b
+//     console.log(c)
+// }
+// multi()
+// function div(a=Number(prompt("Enetr a div number:"),b=Number(prompt("Enter a div Value:")))){
+//     let d=a/b
+//     console.log((d))
+// }
+// div()
+// function remai(a=Number(prompt("Enetr a remai number:"),b=Number(prompt("Enter a remai Value:")))){
+//     let r=a%b
+//     console.log(r)
+// }
+// remai()
+// function sq(a=Number(prompt("Enetr a sq number:"),b=Number(prompt("Enter a sq Value:")))){
+//     let w=a**b
+//     console.log(w)
+// }
+// sq()
+
+////
+// function con(){
+//     let  a ="chethan"
+//     let b=" D B"
+//     let c=a.concat(b)
+//     console.log(c)
+// }
+// con()
+// function cha(){
+//     let a="chetahn"
+//     let b=a.charAt(1)
+//     console.log(b)
+// }cha()
+// function startw(){
+//     let a="virat"
+//     let d= a.startsWith("vir")
+//     console.log(d)
+// }startw()
+// function endw(){
+//     let a="kholi"
+//     let c=a.endsWith("chet")
+//     console.log(c)
+// }endw()
+
+// function include(){
+//     let a="ravi shastry"
+//     let f=a.includes(" ")
+//     console.log(f)
+// }include()
+
+// function indexf(){
+//     let a ="Toxic"
+//     let w=a.indexOf("o")
+//     console.log(w)
+// }indexf()
+
+// function lasti(){
+//     let a ="chethan"
+//     let d=a.lastIndexOf("n")
+//     console.log(d)
+// }lasti()
+
+// function pade(){
+//     let a ="ramayana"
+//     let c=a.padEnd(20,"X")
+//     console.log(c)
+// }pade()
+
+
+// function pads(){
+//     let a ="Varanasi"
+//     let c=a.padStart(15,"X")
+//     console.log(c)
+// }pads()
+
+// function rep(){
+//     let a ="cricket"
+//     let w=a.repeat(3)
+//     console.log(w)
+// }rep()
+
+//////////////////////////
+// let a =100
+// let b=200
+// function add(){
+//     let c=a+b
+//     console.log(c)
+// }
+// add( )
+// function sub(){
+//     let d=b-a
+//     console.log(d)
+// }sub()
+
+// function mult(){
+//     let d=a*b
+//     console.log(d)
+// }mult()
+// function div(){ 
+//      let d=a/b
+//     console.log(d)
+// }div()
+
+// function rem(){ 
+//      let d=a%b
+//     console.log(d)
+// }rem()
+
+// function pow(){ 
+//      let d=a**b
+//     console.log(d)
+// }pow()
+
+// function incr(){ 
+//      let d=a++
+//     console.log(d)
+// }incr()
+
+// function decr(){ 
+//      let d=b--
+//     console.log(d)
+// }decr()
+
+// function posincr(){ 
+//      let d=++a
+//     console.log(d)
+// }posincr()
+
+// function posdecr(){ 
+//      let d=--b
+//     console.log(d)
+// }posdecr()
