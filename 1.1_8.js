8ad14c1c4feb94032d4a38927859933ad5b170c3
CHETHAN D B <chethandb.cse@gmail.com>
2026-09-07 22:47:33 +0530

    Add files via upload

---
+++ 1.1_8.js
@@ -0,0 +1,99 @@
+// function fullname(a,b){
+//     // let x=a.concat(b)
+//     let x=a+b
+//     console.log(x)
+// }
+// // fullname("chethan"," D B")
+// fullname(1,20)
+// function assign(a,c){
+//     let x=a+=c
+//     console.log(x)
+// }
+// assign(Number(prompt("Enter a:"),Number(prompt("Enter b:"))))
+
+// function assign(s,d){
+//     let x=s-=d
+//     console.log(x)
+// }
+// assign(Number(prompt("Enter a:"),Number(prompt("Enter b:"))))
+
+// function assign(q,w){
+//     let x=q*=w
+//     console.log(x)
+// }
+// assign(Number(prompt("Enter a:"),Number(prompt("Enter b:"))))
+
+// function assign(a,c){
+//     let x=a/=c
+//     console.log(x)
+// }
+// assign(Number(prompt("Enter a:"),Number(prompt("Enter b:"))))
+
+// function assign(e,r){
+//     let x=e**r
+//     console.log(x)
+// }
+// assign(Number(prompt("Enter a:"),Number(prompt("Enter b:"))))
+
+// function assign(t,y){
+//     let x=t%y
+//     console.log(x)
+// }
+// assign(Number(prompt("Enter a:"),Number(prompt("Enter b:"))))
+////////////
+
+// function full_name(a="chethan",b="D B"){
+//     let x=a.concat(b)
+//     console.log(x)
+// } full_name("Virat","Koli") // Important
+
+//////////
+// function first_name1(){
+//     let a="Chethan"
+//     return(a)
+// }
+// let name=first_name1()
+// function last_name1(){
+//     let b="D B"
+//     return(b)
+// }
+// let last=last_name1()
+// function full_name1(){
+//     console.log(`hello all my full name is ${name}${last}`)
+// }full_name1()
+
+// //////////////
+
+// function adds(){
+//     let a=1
+//     return(a)
+// }
+// let ad=adds()
+
+// function add(){
+//     let c=4
+//     return (c)
+// }
+// let sd=add()
+
+// function res(){
+//     let b=ad+sd
+//     console.log(b)
+// }res()
+
+///////////////////
+
+// function add(){
+//     let a=1
+//     let b=10
+//     return (a)
+// }
+// let z1=add()
+// console.log(z1)
+
+let greet=()=>{
+    console.log("I AM Chetahn")
+    console.log("I am a Full Stack Developer")
+    console.log("My Hobbies : Watching Series")
+}
+greet()
