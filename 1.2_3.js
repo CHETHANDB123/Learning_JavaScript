a409c8480a0db90901936dea3d72c3c10fe00a69
CHETHAN D B <chethandb.cse@gmail.com>
2026-09-17 23:14:35 +0530

    Add files via upload

---
+++ 1.2_3.js
@@ -0,0 +1,122 @@
+//wrt arry of 5 friits
+//print range 10-1
+//
+// let ar=['apple','mango','banana','pinapple','watermellon']
+// console.log(ar)
+// for(i=10;i<=1;i++){
+//     console.log(i)
+// }
+// //print a sum of even range 100-1
+// for(i=100;i>1;i++){
+//     if(i%2==0){
+
+//     }
+// // }
+// //wrt pro print a tables of x range a to b
+// let x=prompt("Enter x:")
+// let a=prompt("Enter a:")
+// let b=prompt("Enter b:")
+
+// if(isNaN(a)||isNaN(b)){
+//     Alert("INvalid")
+//     }
+// else if(a>b)
+// {
+//     console.log(`x*${a}=${a*b}`)
+// }
+// else if(b>a){
+//     console.log(`x*${b}=${a*b}`)
+// }
+
+
+//sum of n number range 100-1
+// let sum=0
+// for(i=100;i>=1;i--){
+//     sum+=i
+// }
+// console.log(sum)
+//wrt reverse string by user
+// let a="chethan"
+// let new_str=''
+// for(i=a;a.length-1;i--){
+    
+// }
+//wrt arr 5 fruits
+// let ar=['apple','mango','banana','pinapple','watermellon']
+// for(ar=0;ar.length-1;ar++){
+   
+//     console.log(ar)
+// }
+//print 10 frd name 
+// let frd=['chethan','guru','akhil','pramod','madhan','arya','naik','shawan','hari','virat']
+// for(let i=0;i<frd.length-1;i++){
+//     console.log(frd[i])
+// }
+
+
+//wrt pro print names of 10 frd revers the array
+// let frd=['chethan','guru','akhil','pramod','madhan','arya','naik','shawan','hari','virat']
+// let new_=[]
+// for(i=frd.length-1;i>0;i--){
+//     new_.push(frd[i])
+// }
+// console.log(frd)
+
+
+// //arr 5 friuts and print capital push new arrray
+// let fruits=['apple','mango','banana','pinapple','watermellon']
+// let new_=[]
+// for(let i=0;i<fruits.length;i++){
+//     new_.push(fruits[i].toUpperCase())
+// }
+// console.log(new_)
+
+// 10 ftuits arr  , if apple is prest arr , print it new arr
+// let fruits=['apple','mango','banana','pinapple','watermellon','grapes','orange','swee paotta']
+// for(i=0;i<fruits.length;i++){
+//     if(friuts[i]==="apple"){
+//         new.push(friits[0].toUpperCase)
+//     }
+// }
+
+//10 fruits in which index values is even , print upper
+// let fruits=['apple','mango','banana','pinapple','watermellon','grapes','orange','swee paotta']
+// for(i=0;i<fruits.length;i++){
+//     if(i%2===0){
+//         console.log(fruits[i].toUpperCase())
+//     }
+//     else{
+//         console.log("Invalid")
+//     }
+// }
+
+//print 10 fruits each element of 1st leeter upper
+// let fruits=['apple','mango','banana','pinapple','watermellon','grapes','orange','swee paotta']
+// for(i=0;i<fruits.length;i++){
+//         console.log(fruits[i].slice(0,1).toUpperCase()+fruits[i].slice(1))
+// }
+
+//wrt pro table 1-5
+// for(let i=1;i<=5;i++){
+//     console.log(`1X${i}=${i*1}`)
+//     for(let j=1;i<=10;i++){
+//     console.log(`1X${i}=${i*1}`)
+// }
+// }
+//wrt pro table range a & b 
+// let a =prompt("Enter a:")
+// let b=prompt("Enter b:")
+// if(a<b){
+// for(let i=a;i<=b;i++){
+//     for(let j=1;j<=10;j++){
+//     console.log(`${i}x${j}=${i*j}`)
+// }
+// }
+// }
+// else{
+// for(let i=a;i>=b;i--){
+//     for(let j=1;j<=10;j++){
+//     console.log(`${i}x${j}=${i*j}`)
+// }
+// }
+// }
