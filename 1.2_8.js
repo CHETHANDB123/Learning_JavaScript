8afc0fc20abdcc9726a1d05fd105b5fe92d30146
CHETHAN D B <chethandb.cse@gmail.com>
2026-09-25 22:35:05 +0530

    Add files via upload

---
+++ 1.2_8.js
@@ -0,0 +1,32 @@
+// ///DOM
+// let one=document.getElementById("one")
+// // console.log(one)
+// one.style.color="red"
+// one.style.textAlign="center"
+// one.style.textDecoration='underline'
+let text=document.getElementById("text")
+let one=document.getElementById("one")
+let two=document.getElementById("two")
+let three=document.getElementById("three")
+let para=document.getElementById("para")
+let but=document.getElementById("but")
+let img=document.getElementById("img")
+let container=document.getElementById("container")
+
+// one.style.textTransform="Capitalize"
+// // console.log(one)
+// one.style.fontSize="30px"
+// two.style.textTransform="Capitalize"
+// two.style.fontSize="60px"
+
+// three.style.textTransform="Capitalize"
+// three.style.fontSize="30px"
+// // img.style.width="50px"
+// // para.style.textalign="justify"
+// container.style.display="flex"
+
+// text.style.dispa
+one.innerText="Hello,"
+two.innerText="heI am chethan"
+three.innerText="I am Full stack developer"
+para.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugiat aperiam, aliquam natus sunt ad ex enim. Repudiandae animi dicta aut vitae id aliquam consequatur libero quasi, a, quas autem?"
